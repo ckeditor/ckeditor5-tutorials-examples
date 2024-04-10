e29f744e2f10e25ae5cad5cdd6da6cb1abb7324e
Illia Sheremetov <i.sheremetov@cksource.com>
2024-04-10 15:09:34 +0200

    Update starter-files and final-project.

--- timestamp-plugin/starter-files/app.js
+++ timestamp-plugin/starter-files/app.js
@@ -3,12 +3,16 @@
  * For licensing, see LICENSE.md.
  */
 
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { List } from '@ckeditor/ckeditor5-list';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { 
+  ClassicEditor,
+  Bold,
+  Essentials,
+  Heading,
+  Italic,
+  Paragraph,
+  List
+} from 'ckeditor5';
+import 'ckeditor5/dist/index.css';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
@@ -21,4 +25,3 @@ ClassicEditor
 	.catch( error => {
 		console.error( error.stack );
 	} );
-	
