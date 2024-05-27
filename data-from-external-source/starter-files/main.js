82197cd33abd1529fd91abbb2405e471fd6b7325
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-27 12:41:43 +0200

    Add the Data from external source starter files.

---
+++ data-from-external-source/starter-files/main.js
@@ -0,0 +1,22 @@
+// main.js
+
+import { ClassicEditor, Bold, Italic, Essentials, Heading, List, Paragraph } from 'ckeditor5';
+import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
+import 'ckeditor5/index.css';
+
+ClassicEditor
+    .create( document.querySelector( '#editor' ), {
+        plugins: [ Essentials, Paragraph, Heading, List, Bold, Italic ],
+        toolbar: [ 'heading', 'bold', 'italic', 'numberedList', 'bulletedList', '|', 'undo', 'redo' ]
+    } )
+    .then( editor => {
+        console.log( 'Editor was initialized', editor );
+
+        CKEditorInspector.attach( { 'editor': editor } )
+
+        // Expose for playing in the console.
+        window.editor = editor;
+    } )
+    .catch( error => {
+        console.error( error.stack );
+    } );
